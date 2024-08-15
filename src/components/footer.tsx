742e2919da116b0f04e47fe2ea6abbe01d8b2e7b
Moh Rizal Alfarizi <alfariiiziiiii@gmail.com>
2024-08-15 17:34:31 +0700

    fix(footer): app name typo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t px-4 py-6 sm:flex-row md:px-6">
       <p className="text-xs text-muted-foreground">
-        &copy; 2024 Jejalin. All rights reserved.
+        &copy; 2024 Jalinan. All rights reserved.
       </p>
       <nav className="flex gap-4 sm:ml-auto sm:gap-6">
         <Link
